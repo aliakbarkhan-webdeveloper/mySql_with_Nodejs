c33c9e737b7d183314f16c013d9f7d210d89cebc
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-23 23:06:12 +0500

    school controller

--- controllers/school.controller.js
+++ controllers/school.controller.js
@@ -44,4 +44,31 @@ const getTeacherById = async (req, res) => {
     });
   }
 };
-module.exports = { getTeachers, getTeacherById };
+
+const createTeacher = async (req, res) => {
+  try {
+    db.query("USE school")
+    const { id, Name, dep_id } =req.body;
+    console.log(Name);
+    //let id, Name, dep_id;
+    // Name = data.Name;
+    // dep_id = data.dep_id;
+    // id = data.id;
+
+    if (!id || !Name || !dep_id) {
+      return res.send("some data is missing");
+    }
+
+    await db.query(`INSERT INTO teachers (id,Name,dep_id) values(?,?,?)`,[id,Name,dep_id]);
+
+    res.send("created successfully");
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "error in get Teachers API",
+      error,
+    });
+  }
+};
+module.exports = { getTeachers, getTeacherById, createTeacher };
