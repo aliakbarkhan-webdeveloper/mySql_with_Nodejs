1c5e5b63273181cc52dad424a98471c142ed41ab
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-23 20:23:40 +0500

    teacher controller

--- controllers/school.controller.js
+++ controllers/school.controller.js
@@ -1,46 +1,47 @@
-const db=require("../config/db.js")
-const getTeachers=async (req,res)=>{
-try {
+const db = require("../config/db.js");
+const getTeachers = async (req, res) => {
+  try {
     db.query("USE school");
-    const data=await db.query("SELECT * FROM teachers");
+    const data = await db.query("SELECT * FROM teachers");
     if (!data) {
-        res.status(404).send({
-            success:false,message:"No, Records"
-        })
-        return;
+      res.status(404).send({
+        success: false,
+        message: "No, Records",
+      });
+      return;
     }
     res.json(data[0]);
-} catch (error) {
+  } catch (error) {
     console.log(error);
     res.status(500).send({
-        success:false,message:"error in get Teachers API",error
-    })
-    
-}
+      success: false,
+      message: "error in get Teachers API",
+      error,
+    });
+  }
 };
 
-const getTeacherById=async(req,res)=>{
+const getTeacherById = async (req, res) => {
+  try {
+    // db.query("USE school");
+    const id = req.params.id;
+    console.log(id);
 
-    try {
-        // db.query("USE school");
-      const id=req.params.id;
-      console.log(id);
-      
-        if (!id) {
-           return res.status(404).send({
-                success:false,message:"need param"
-            })
-        
-        }
-        const data=await db.query(`select * FROM teachers WHERE id=${id}`)
-        res.json(data[0]);
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({
-            success:false,message:"error in get Teachers API",error
-        })
-        
+    if (!id) {
+      return res.status(404).send({
+        success: false,
+        message: "need param",
+      });
     }
-
-}
-module.exports={getTeachers,getTeacherById};
+    const data = await db.query(`select * FROM teachers WHERE id=${id}`);
+    res.json(data[0]);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "error in get Teachers API",
+      error,
+    });
+  }
+};
+module.exports = { getTeachers, getTeacherById };
