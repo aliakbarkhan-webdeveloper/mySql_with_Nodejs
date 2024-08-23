cf5a3c7512c0825a8ccbf27dc506f4e05dbf5b09
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-23 23:06:20 +0500

    Server

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const app = express();
 const router=require("./routes/school.route.js");
 const PORT = 4000;
 
+
 app.use(express.json());
-app.use(router)
+app.use(express.urlencoded({extended:true}));
+app.use(router);
 sqlpool.query("select 1").then(
   () =>{ console.log("DB is connected");
     app.listen(PORT, () => console.log("server is running on PORT " + PORT));
