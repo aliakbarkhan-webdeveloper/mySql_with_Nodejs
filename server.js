078ec0e32c9bd5030ddcbb70322212be30a34c72
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-23 16:51:05 +0500

    server

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
-const express=require("express");
-const app=express();
-const PORT=4000;
+const express = require("express");
+const sqlpool = require("./config/db.js");
+const app = express();
+const router=require("./routes/school.route.js");
+const PORT = 4000;
+
 app.use(express.json());
+app.use(router)
+sqlpool.query("select 1").then(
+  () =>{ console.log("DB is connected");
+    app.listen(PORT, () => console.log("server is running on PORT " + PORT));
 
-
-
-
-
-
-
-
-
-
-
-
-app.listen(PORT,()=>console.log("server is running on PORT "+PORT))
+  }).catch((e)=>{console.log(e)});
+  
