99dddf8d952c666edebc29db676fa57b84bd1913
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-23 16:51:24 +0500

    db configuration

--- config/db.js
+++ config/db.js
@@ -1,13 +1,12 @@
-const mysql=require("mysql2");
+const mysql=require("mysql2/promise");
 
 const sqlpool=mysql.createPool(
 {
-host:"localost",
+host:"localhost",
 user:"root",
 password:"password",
 database:"school",
-},()=>console.log("connection established")
-
+}
 )
 
 module.exports=sqlpool;
