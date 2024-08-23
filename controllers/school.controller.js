0ef2b0208309068dbdb9be8c361a4a8bfed113f7
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-23 20:22:58 +0500

    teachers controller

--- controllers/school.controller.js
+++ controllers/school.controller.js
@@ -19,4 +19,28 @@ try {
 }
 };
 
-module.exports=getTeachers;
+const getTeacherById=async(req,res)=>{
+
+    try {
+        // db.query("USE school");
+      const id=req.params.id;
+      console.log(id);
+      
+        if (!id) {
+           return res.status(404).send({
+                success:false,message:"need param"
+            })
+        
+        }
+        const data=await db.query(`select * FROM teachers WHERE id=${id}`)
+        res.json(data[0]);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            success:false,message:"error in get Teachers API",error
+        })
+        
+    }
+
+}
+module.exports={getTeachers,getTeacherById};
