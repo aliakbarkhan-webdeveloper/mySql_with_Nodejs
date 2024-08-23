ed69f3746bb0a009306339df676638c94c48d1e5
Ali Akbar Khan <122238636+aliakbarkhan-webdeveloper@users.noreply.github.com>
2024-08-23 23:06:01 +0500

    school route

--- routes/school.route.js
+++ routes/school.route.js
@@ -1,10 +1,11 @@
 const router=require("express").Router();
-const {getTeachers,getTeacherById}=require("../controllers/school.controller.js");
+const {getTeachers,getTeacherById,createTeacher}=require("../controllers/school.controller.js");
 
 //router for getting all data
 router.get("/teacher",getTeachers)
 
 //router for getting data by teacher id
 router.get("/teacher/:id",getTeacherById)
-
+//router for create teacher info
+router.post("/teacher",createTeacher);
 module.exports=router;
